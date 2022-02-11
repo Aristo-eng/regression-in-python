cf055be8cf88bb5fe89663977138896c2e01d045
Aristo-eng <ribeus.munezero@yahoo.com>
2022-02-11 10:42:50 +0200

    Added labels

--- regression.py
+++ regression.py
@@ -5,6 +5,7 @@ warnings.filterwarnings('ignore')
 import pandas as pd
 import numpy as np
 import quandl as quandl
+import math
 
 #Get data set
 data = quandl.get('WIKI/GOOGL')
@@ -22,5 +23,17 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open'])/ df['Adj. Open'] * 100.0
 
 # Select the only feautures we care about
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+print(df.head(10))
+# feature and label
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+# instead of get rid of data, replace nans by outliers
 
-print(df.head())
+
+forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
+# we are using data from 10 days ago to predict the forecast ...feel free to change ...
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+print(df.head(5))
